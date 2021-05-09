ee8f93ff01a9493231a9c2d98f736e1132295294
Hamza <hamzzza56@gmail.com>
2021-05-09 23:45:04 +0200

    Aplica dashboard y crea plantilla general

--- papyrus/src/main/java/com/papyrus/autor/AutorService.java
+++ papyrus/src/main/java/com/papyrus/autor/AutorService.java
@@ -1,13 +1,9 @@
 package com.papyrus.autor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AutorService
 {
-    @Autowired
-    private AutorRepository repo;
-
     
 }
--- papyrus/src/main/java/com/papyrus/libro_editorial/LibroEditorialService.java
+++ papyrus/src/main/java/com/papyrus/libro_editorial/LibroEditorialService.java
@@ -1,13 +1,9 @@
 package com.papyrus.libro_editorial;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LibroEditorialService
 {
-    @Autowired
-    private LibroEditorialRepository repo;
-
     
 }
